3af03406d949af18f428b07f492afb06e3fa85f7
asha-gc <56806594+asha-gc@users.noreply.github.com>
2021-04-09 15:27:34 -0500

    Add files via upload

---
+++ Usuarios.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author asha2
+ */
+public class Usuarios {
+    private String usuario;
+    private String contrasenia;
+
+    public Usuarios(String usuario, String contrasenia) {
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+}
